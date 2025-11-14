bda494ba54461224902b92660115bfbb15da91fe
situu22 <maynoudegbe@gmail.com>
2025-11-14 15:48:54 +0100

    change awsip.py

--- test/mock_test.py
+++ test/mock_test.py
@@ -4,13 +4,13 @@ from src.awsip import find_aws_ip
 
 
 class MockTest(unittest.TestCase):
-    @patch('src.awsip.download_aws_ip_set')  # Make sure this path is correct
+    @patch("src.awsip.download_aws_ip_set")  # Make sure this path is correct
     def test_withmock(self, mock_download):
         mock_data = {
             "ip_prefix": "3.4.12.4/32",
             "region": "eu-west-1",
             "service": "AMAZON",
-            "network_border_group": "eu-west-1"
+            "network_border_group": "eu-west-1",
         }
         mock_download.return_value = [mock_data]
 
@@ -18,4 +18,3 @@ class MockTest(unittest.TestCase):
 
         mock_download.assert_called_once()
         self.assertEqual(len(result), 0)
-
